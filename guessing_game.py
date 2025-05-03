dd2a3751a412209fc08d7e2e12be928344adc543
Thamasha1234 <dinithithamasharajapaksha2003@gmail.com>
2025-05-03 17:34:43 +0530

    Update guessing_game.py

--- guessing_game.py
+++ guessing_game.py
@@ -1,6 +1,6 @@
 import random
-number = random.randint(1, 100)
-print("Guess a number between 1 and 100")
+number = random.randint(1, 50)
+print("Guess a number between 1 and 50")
 guess = int(input())
 if guess == number:
     print("You win!")
