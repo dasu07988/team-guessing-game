8f9a9ea002293f32c7b0bde0be919fe02f51ff63
Viraji62 <41-bit-0004@kdu.ac.lk>
2025-05-03 19:44:14 +0530

    Update guessing_game.py

--- guessing_game.py
+++ guessing_game.py
@@ -1,11 +1,11 @@
 import random
 while True:
-       number = random.randint(1, 10)
-       print("Guess a number between 1 and 10")
-       guess = int(input())
-       if guess == number:
+ number = random.randint(1, 10)
+ print("Guess a number between 1 and 10")
+ guess = int(input())
+          if guess == number:
           f guess < number:
-       print("Too low!")
+          print("Too low!")
    elif guess > number:
        print("Too high!")
    elif guess == number:
